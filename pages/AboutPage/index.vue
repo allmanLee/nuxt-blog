75e7cf6beae8a82643fce126d0d5f595d2e53997
leeyoujun <muenzz119@naver.com>
2021-08-12 22:19:35 +0900

    fix: fix ui error someting is not important

--- pages/AboutPage/index.vue
+++ pages/AboutPage/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" sm="6" class="pr-14">
+      <v-col>
         <h4>LEEYOUJUN</h4>
         <h1 class="mb-12">이유준</h1>
         <v-row justify="center">
